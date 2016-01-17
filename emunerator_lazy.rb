8996d10a3d0eeaa2ece57bab4fda454214135f89
Andrew Scott <amscott@openmailbox.org>
2016-01-17 00:32:35 +0000

    _

--- emunerator_lazy.rb
+++ emunerator_lazy.rb
@@ -1,10 +1,7 @@
 class DoTheLazy
-
   def do_the_lazy(array_input)
     Enumerator::Lazy.new(array_input) do |yielder, value|
       yielder << value
     end
   end
 end
-
-DoTheLazy.new.do_the_lazy([1,2,3,4,5,6])
